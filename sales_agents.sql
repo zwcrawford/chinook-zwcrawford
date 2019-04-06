a05b103bbbd4f1bcb0a81624a598e2cc8f975236
Zac Crawford <zwcrawford@gmail.com>
2019-04-06 14:00:50 -0500

    Part 4

--- sales_agents.sql
+++ sales_agents.sql
@@ -1,3 +1,4 @@
+-- Provide a query showing only the Employees who are Sales Agents.
 SELECT e.FirstName + ' ' + e.LastName AS FullName, e.Title, FORMAT(e.HireDate, 'dd/MM/yyyy') AS HireDate
   FROM Employee e
  WHERE Title = 'Sales Support Agent';
