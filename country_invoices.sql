764cd883f1b68c492b978ccda68029f4909e6067
Zac Crawford <zwcrawford@gmail.com>
2019-04-07 17:31:11 -0500

    Update country_invoices.sql

--- country_invoices.sql
+++ country_invoices.sql
@@ -4,7 +4,5 @@
   SELECT DISTINCT i.BillingCountry AS 'Billing Country', COUNT(il.InvoiceLineId) AS 'Invoice Count'
     FROM InvoiceLine il LEFT JOIN Track t ON il.TrackId = t.TrackId
 		 LEFT JOIN Invoice i ON i.InvoiceId = il.InvoiceId
-		 LEFT JOIN Album al ON al.AlbumId = t.AlbumId
-		 LEFT JOIN Artist ar ON ar.ArtistId = al.ArtistId
 GROUP BY i.BillingCountry
 ORDER BY i.BillingCountry;
