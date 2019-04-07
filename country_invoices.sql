dff9091af2ea7884646ffdfe8dcae6cb7355e77c
Zac Crawford <zwcrawford@gmail.com>
2019-04-07 17:05:06 -0500

    Part 14

---
+++ country_invoices.sql
@@ -0,0 +1,10 @@
+
+-- Provide a query that shows the # of invoices per country. HINT: GROUP BY   
+ 
+  SELECT DISTINCT i.BillingCountry AS 'Billing Country', COUNT(il.InvoiceLineId) AS 'Invoice Count'
+    FROM InvoiceLine il LEFT JOIN Track t ON il.TrackId = t.TrackId
+		 LEFT JOIN Invoice i ON i.InvoiceId = il.InvoiceId
+		 LEFT JOIN Album al ON al.AlbumId = t.AlbumId
+		 LEFT JOIN Artist ar ON ar.ArtistId = al.ArtistId
+GROUP BY i.BillingCountry
+ORDER BY i.BillingCountry;
